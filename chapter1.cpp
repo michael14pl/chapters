47fce17af8ff4c477e967d848342032d05ffd2f4
michael14pl <michael14pl@gmail.com>
2021-02-09 15:00:42 +0100

    mod2

--- chapter1.cpp
+++ chapter1.cpp
@@ -4,10 +4,21 @@ class Chap
 {
 public:
 	Chap(int n) : number(n)
-	{ std::cout << "Constructor chapter " << n << std::endl; }
-	~Chap(){ std::cout << "Destructor called" << std::endl; }
+	{
+	 std::cout << "Constructor chapter " << n << std::endl;
+	}
+	~Chap()
+	{
+	std::cout << "Destructor called" << std::endl;
+	}
+
+	void setName(std::string name)
+	{
+	std::string chapterName = name;
+	}
 private:
 	int number = 0;
+	std::string name = "no name";
 };
 
 int main()
