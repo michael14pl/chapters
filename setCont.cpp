2de749dadeacda7b9d96732e0a4b9991d098df26
michael14pl <michael14pl@gmail.com>
2021-02-11 07:29:53 +0100

    setCont refactor

--- setCont.cpp
+++ setCont.cpp
@@ -1,11 +1,4 @@
 #include <iostream>
-#include <utility>
-#include <cstring>
-
-std::string setChapterTitle(std::string con)
-{
-	return con;
-}
 
 std::string addContents(std::string oldCont, std::string newCont)
 {
